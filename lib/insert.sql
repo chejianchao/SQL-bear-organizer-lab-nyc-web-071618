01097c48f80f7a9bdaa163aede31a031a8ea5bc0
chejianchao <820992376@qq.com>
2018-07-22 22:18:36 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+insert into bears(id, name) values(1, "Mr. Chocolate");
+insert into bears(id, name) values(2, "Mr. Chocolate");
+insert into bears(id, name) values(3, "Mr. Chocolate");
+insert into bears(id, name) values(4, "Mr. Chocolate");
+insert into bears(id, name) values(5, "Mr. Chocolate");
+insert into bears(id, name) values(6, "Mr. Chocolate");
+insert into bears(id, name) values(7, "Mr. Chocolate");
+insert into bears(id) values(8);
